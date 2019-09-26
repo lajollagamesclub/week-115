a946fac445d9ce9468884858f2a3bf8cafb8b441
Cameron Reikes <cameronreikes@gmail.com>
2019-09-26 15:54:31 -0700

    Add age progress bar script

---
+++ src/AgeProgressBar.gd
@@ -0,0 +1,13 @@
+extends ProgressBar
+
+const game_state = preload("res://game_state.tres")
+
+func _ready():
+	max_value = game_state.max_time
+	game_state.connect("time_left_changed", self, "_time_left_changed")
+
+func _process(delta):
+	game_state.time_left -= delta*20.0
+
+func _time_left_changed(new_time_left):
+	value = new_time_left
